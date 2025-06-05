f64794645ab8e18ed3adf47715c73a7ab40d7142
KING DAVID <otwanemark254@gmail.com>
2025-06-06 02:48:55 +0300

    trying ti connect my modules

---
+++ src/index.ts
@@ -0,0 +1,2 @@
+// import Signup from './modules/signUp.module';
+// TODO: Use Signup here or remove the import if not needed
--- src/modules/signUp.module.ts
+++ src/modules/signUp.module.ts
@@ -19,7 +19,7 @@ interface SignUp {
 }
 
 //Dom
-const firstNameInput = document.getElementById('firstName') as HTMLInputElement;
+const firstNameInput = document.getElementById("firstName") as HTMLInputElement;
 const lastNameInput = document.getElementById("lastName") as HTMLInputElement;
 const emailInput = document.getElementById("email") as HTMLInputElement;
 const phoneNumberInput = document.getElementById("phoneNumber") as HTMLInputElement;
@@ -29,10 +29,7 @@ const zipCodeInput = document.getElementById("lastName") as HTMLInputElement;
 const passwordInput = document.getElementById("lastName") as HTMLInputElement;
 const signUpButton = document.getElementById("signUpBtn") as HTMLButtonElement;
 
-
-
 let signUpNewUser: SignUp[] = JSON.parse(localStorage.getItem("signUpNewUser") || "[]");
-
 
 /**
  * sign up function
@@ -44,22 +41,22 @@ const signUp = () => {
 		firstName: firstNameInput.value,
 		lastName: lastNameInput.value,
 		email: emailInput.value,
-        phoneNumber: phoneNumberInput.value,
-        password: passwordInput.value ,
+		phoneNumber: phoneNumberInput.value,
+		password: passwordInput.value,
 		address: {
 			street: streetInput.value,
 			city: cityInput.value,
-			zipCode: zipCodeInput.value
-		}
-    };
-    if (!signUp.firstName || !signUp.lastName || !signUp.email || !signUp.phoneNumber || !signUp.password || !signUp.address) {
-        alert('All the input fields are required!');
-        return;
-    }
+			zipCode: zipCodeInput.value,
+		},
+	};
+	if (!signUp.firstName || !signUp.lastName || !signUp.email || !signUp.phoneNumber || !signUp.password || !signUp.address) {
+		alert("All the input fields are required!");
+		return;
+	}
 	// Save the new user to localStorage
 	signUpNewUser.push(signUp);
 	localStorage.setItem("signUpNewUser", JSON.stringify(signUpNewUser));
-	alert('Sign up successful!');
-}
+	alert("Sign up successful!");
+};
 
-signUpButton.addEventListener('click', signUp);
+signUpButton.addEventListener("click", signUp);
